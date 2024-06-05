a9158c5be410fa91136d6a5086490e8ffc2fe60c
skimo <skimo@SKIMOCHK>
2024-06-05 11:26:13 -0700

    .

--- Test/Services/GeneratePdfService.cs
+++ Test/Services/GeneratePdfService.cs
@@ -59,5 +59,62 @@ namespace Test.Services
             return report;
         }
 
+        //AQUI OBTIENE TODA LA INFO EN UNA SOLA PAGINA
+        public async Task<Document> GeneratePdfQuestV2()
+        {
+
+            var clientes = await _clientesService.GetClientesAsync();
+
+            var report = Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Margin(50);
+                    page.Size(PageSizes.A4);
+                    page.PageColor(Colors.White);
+                    page.DefaultTextStyle(x => x.FontSize(12));
+
+                    page.Header().AlignCenter()
+                    .Text("Factura : 80025-63")
+                    .SemiBold().FontSize(24).FontColor(Colors.Blue.Darken2);
+
+                    page.Content().Padding(25)
+                    .Table(table =>
+                    {
+                        table.ColumnsDefinition(columns =>
+                        {
+                            columns.RelativeColumn();
+                            columns.RelativeColumn();
+                            columns.RelativeColumn();
+                        });
+
+
+
+                        table.Header(header =>
+                        {
+                            header.Cell().Text("Nombre");
+                            header.Cell().Text("Apellido");
+                            header.Cell().Text("Email");
+
+                        });
+
+                        foreach (var cliente in clientes)
+                        {
+                            table.Cell().Text(cliente.Nombre);
+                            table.Cell().Text(cliente.Apellido);
+                            table.Cell().Text(cliente.Email);
+                        }
+
+                    });
+
+
+                });
+
+
+            });
+
+            return report;
+        }
+
     }
 }
